f7ae859b2ec07171847aa556cfd67d39c5988980
Om Singh Chauhan <ri.oomee2@gmail.com>
2026-05-24 08:11:27 +0530

    Fix database connection variables

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 // Connect to MySQL
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST || process.env.DB_HOST,
-  port: process.env.MYSQLPORT || process.env.DB_PORT || 3306,
+  port: process.env.MYSQLPORT || process.env.DB_PORT,
   user: process.env.MYSQLUSER || process.env.DB_USER,
   password: process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
   database: process.env.MYSQLDATABASE || process.env.DB_NAME
